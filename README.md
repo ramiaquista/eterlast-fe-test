5d0050928f621c1d8af656089fa840fe4144a239
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-03-22 14:07:00 -0300

    Update Minting Btn Handler, Add descriptive README.md file

---
+++ README.md
@@ -0,0 +1,31 @@
+# Eterlast FE Test
+
+## Fake Minting website for Eterlast FE Test
+
+![screenshot](./read-img.png)
+
+## Built With
+
+- React
+- Web3.js
+- TailwindCss
+- Vite
+
+## Getting Started
+
+- To get a local copy of the repository please run the following commands on your terminal:
+   ```
+   git clone https://github.com/ramiaquista/eterlast-fe-test
+   ```
+- Go to the cloned directory with `cd eterlast-fe-test`
+- Run `npm install` to install all the necessary packages
+- After installing everything, you can run now the website with `npm run dev`
+
+
+## Author
+
+👤 **Ramiro Aquistapace**
+
+- GitHub: [@ramiaquista](https://github.com/ramiaquista)
+- Twitter: [@ramiaq1](https://twitter.com/ramiaq1)
+- LinkedIn: [RamiroAquistapace](https://www.linkedin.com/in/ramiro-aquistapace-32b61b204/)
--- src/components/Wallet.jsx
+++ src/components/Wallet.jsx
@@ -46,7 +46,7 @@ const Wallet = () => {
         break;
       case '5':
         setWalletNetWork('Goerli Network');
-        break; 
+        break;
       default:
         console.log('This is an unknown network.');
     }
@@ -63,20 +63,21 @@ const Wallet = () => {
 
   const mintBtnHandler = () => {
     const mintingData = {
-        creator_wallet_id: defaultAccount,
-        creator_network: walletNetWork,
-        assets: [
+      creator_wallet_id: defaultAccount,
+      creator_network: walletNetWork,
+      assets: [
         {
-        asset_id: 'ab32809412jmcdsa',
-        name: 'Maradona',
-        picture: 'https://i.pinimg.com/originals/9f/3d/23/9f3d2364f50e94e1494dafaf520c45fd.jpg',
-        description: 'Maradona Iconic Photo',
-        collection: 'Argentinian Futbol',
-        supply: '1',
-        date_of_creation: 'Mar 22',
+          asset_id: 'ab32809412jmcdsa',
+          name: 'Maradona',
+          picture:
+            'https://i.pinimg.com/originals/9f/3d/23/9f3d2364f50e94e1494dafaf520c45fd.jpg',
+          description: 'Maradona Iconic Photo',
+          collection: 'Argentinian Futbol',
+          supply: '1',
+          date_of_creation: 'Mar 22',
         },
-        ]
-        }
+      ],
+    };
     console.log(mintingData);
   };
 
@@ -88,8 +89,27 @@ const Wallet = () => {
 
   window.ethereum.on('chainChanged', chainChangeHandler);
 
-  return (
-    <div className="mt-10">
+  const mintNftButton = () => {
+    return (
+      <div>
+        <div className="flex flex-col">
+          <h3 className="font-light text-white">NetWork: {walletNetWork}</h3>
+          <h3 className="font-light text-white">Address: {defaultAccount}</h3>
+          <h3 className="font-light text-white">Balance: {userBalance}</h3>
+        </div>
+        <button
+          type="button"
+          onClick={mintBtnHandler}
+          className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+        >
+          <p className="text-white text-base font-semibold">{mintBtnText}</p>
+        </button>
+      </div>
+    );
+  };
+
+  const connectWalletButton = () => {
+    return (
       <button
         type="button"
         onClick={connectWalletHandler}
@@ -97,20 +117,14 @@ const Wallet = () => {
       >
         <p className="text-white text-base font-semibold">{connButtonText}</p>
       </button>
-      <div className="flex flex-col">
-        <h3 className="font-light text-white">NetWork: {walletNetWork}</h3>
-        <h3 className="font-light text-white">Address: {defaultAccount}</h3>
-        <h3 className="font-light text-white">Balance: {userBalance}</h3>
-      </div>
+    );
+  };
+
+  return (
+    <div className="mt-10">
+      {defaultAccount ? mintNftButton() : connectWalletButton()}
 
       <p className="text-white text-base font">{errorMessage}</p>
-      <button
-        type="button"
-        onClick={mintBtnHandler}
-        className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-      >
-        <p className="text-white text-base font-semibold">{mintBtnText}</p>
-      </button>
     </div>
   );
 };
